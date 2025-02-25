b9c4e57c71bbd0489a8f96e42566865d849cc192
Weslley <weslleyfaustino2013@gmail.com>
2025-02-24 21:15:17 -0300

    explicação ternaria

--- src/weslley/br/Main.java
+++ src/weslley/br/Main.java
@@ -5,6 +5,12 @@ public class Main {
 
 	public static void main(String[] args) {
 	
+		Integer numero = 19;
+		
+		numero = numero > 10 && numero < 20 ? 30 : 40;
+		
+		System.out.println(numero);
+		
 
 	}
 
